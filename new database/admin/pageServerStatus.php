87eb3b24167cdfa117c0d7c5acdffefe21ec53d4
admin panel cbytes <58211048+cbytes@users.noreply.github.com>
2019-12-30 01:55:04 +0530

    happy
    I'm happy

--- new database/admin/pageServerStatus.php
+++
@@ -1,7 +0,0 @@
-<?php
-	$currDir = dirname(__FILE__);
-	require("{$currDir}/incCommon.php");
-
-	$GLOBALS['page_title'] = $Translation['server status'];
-	include("{$currDir}/incHeader.php");
-	include("{$currDir}/incFooter.php");
